7881a1d469a73350a86dad16e21bcb31fdd1b0d1
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-07 09:51:19 +0700

    tast 2

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -6,9 +6,16 @@ void main(List<String> arguments) {
     printUsage();
   } else if(arguments.first == 'version') {
     print('CLI Version: $version');
+  } else if(arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     print('Unknown command: ${arguments.first}');
   }
+}
+
+void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
+  print('searchWikipedia received arguments: $arguments');
 }
 
 void printUsage() { // Add this new function
