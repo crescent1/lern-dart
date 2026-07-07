f6958b526d845ade1ecdd3272577f28dd863172c
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-07 10:20:45 +0700

    test search'

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+
 
 const version = '0.0.1'; // Add this line
 
@@ -14,9 +17,22 @@ void main(List<String> arguments) {
   }
 }
 
-void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
-  print('searchWikipedia received arguments: $arguments');
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
 }
+
 
 void printUsage() { // Add this new function
   print(
