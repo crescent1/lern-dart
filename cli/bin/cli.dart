a4fb6bc4b7c0fe4028a2ce027e7f6081f0251568
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-09 08:32:56 +0700

    update library

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -7,8 +7,8 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) async { // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  await commandRunner.run(arguments); // Call its run method, awaiting its Future<void>
 }
 
 void searchWikipedia(List<String>? arguments) async {
