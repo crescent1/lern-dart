fc7b571efd40d450b4eb3131963f5a3ee03fbc19
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-10 08:54:52 +0700

    chore: chapter 6

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -6,9 +6,19 @@ import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1'; // Add this line
 
-void main(List<String> arguments) async { // main is now async and awaits the runner
-  var commandRunner = CommandRunner()..addCommand(HelpCommand());
-  await commandRunner.run(arguments); // Call its run method, awaiting its Future<void>
+void main(List<String> arguments) {
+  // [Step 6 update] Add onError method
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
 
 void searchWikipedia(List<String>? arguments) async {
