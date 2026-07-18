2b93c72ec8435520c2380e53874fdf1a616c428e
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-18 09:16:24 +0700

    chore: chapter 8

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -7,8 +7,10 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) {
-  // [Step 6 update] Add onError method
   var commandRunner = CommandRunner(
+    onOutput: (String output) async {
+      await write(output);
+    },
     onError: (Object error) {
       if (error is Error) {
         throw error;
