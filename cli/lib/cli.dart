515a1b1e2dc0899cc276b697355a5164f73e40cf
Syaifudin Zuhri <szuhri1@gmail.com>
2026-07-18 10:34:48 +0700

    chore: chapter 12

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,3 +1,39 @@
+import 'package:cli/cli.dart';
+import 'package:command_runner/command_runner.dart';
+
+
+ export 'src/commands/get_article.dart';
+ export 'src/commands/search.dart';
+ export 'src/logger.dart';
+
+
 int calculate() {
   return 6 * 7;
+}
+
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+              print(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  app.run(arguments);
 }
